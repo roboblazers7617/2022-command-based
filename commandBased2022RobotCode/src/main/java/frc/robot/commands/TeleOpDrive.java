41c1d0905b010b033ffabf6eeae2fe8123a16585
Cephandrius42 <94200651+Cephandrius42@users.noreply.github.com>
2022-02-05 16:52:32 -0500

    Ah! please keep file structure.
    This is my rewirte of the initial timed robot code.

---
+++ commandBased2022RobotCode/src/main/java/frc/robot/commands/TeleOpDrive.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+
+public class TeleOpDrive extends CommandBase {
+  private final Supplier<Double> m_xSpeed;
+  private final Supplier<Double> m_ySpeed;
+  private final Supplier<Double> m_zRotation;
+  private final Supplier<Double> m_speedModulator;
+  private final Drivetrain m_drivetrain;
+
+  /** Creates a new TeleOpDrive. */
+  public TeleOpDrive(Drivetrain drivetrain, Supplier<Double> xSpeed, Supplier<Double> ySpeed, Supplier<Double> zRotation, Supplier<Double> speedModulator) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_drivetrain = drivetrain;
+    m_xSpeed = xSpeed;
+    m_ySpeed = ySpeed;
+    m_zRotation = zRotation;
+    m_speedModulator = speedModulator;
+    addRequirements(m_drivetrain);
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_drivetrain.drive(m_xSpeed.get()*m_speedModulator.get(), m_ySpeed.get()*m_speedModulator.get(), m_zRotation.get()*m_speedModulator.get());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
