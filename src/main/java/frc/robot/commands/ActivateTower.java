0929e2be3e8345591ed6802fdbfcb71a9d9cd236
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2022-02-12 13:41:24 -0500

    created activateTower.java

---
+++ src/main/java/frc/robot/commands/ActivateTower.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.Tower;
+
+public class ActivateTower extends CommandBase {
+  private Tower tower;
+  private boolean sesor;
+  private boolean finished;
+  /** Creates a new ToggleTower. */
+  /*
+
+  Both tower motors will spin until sensor is set to false(the laser is tripped)
+
+
+
+
+
+  */
+  public ActivateTower(Tower tower) {
+    addRequirements(tower);
+    this.tower = tower;
+    finished = false;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    tower.setSpeedLower(Constants.TOWER_SPEED);
+    tower.setSpeedUpper(Constants.TOWER_SPEED);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(!sesor){
+      tower.setSpeedLower(0);
+      tower.setSpeedUpper(0);
+      finished = true;
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return finished;
+  }
+}
