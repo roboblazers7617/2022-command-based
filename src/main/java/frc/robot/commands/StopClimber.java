fc478c8f9f4a2e0b141bf45588dab5a576a4f997
SamohAwesome <72154565+SamohAwesome@users.noreply.github.com>
2022-03-02 17:23:47 -0500

    nothing will go wrong
    I swear

--- src/main/java/frc/robot/commands/StopClimber.java
+++ src/main/java/frc/robot/commands/StopClimber.java
@@ -37,6 +37,6 @@ public class StopClimber extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
