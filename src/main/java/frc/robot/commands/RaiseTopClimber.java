d1a035d97b27b9e5aebc282f53aaa0f86cbf280a
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2022-03-09 16:13:12 -0500

    set the speed for climber to zero after command ends

--- src/main/java/frc/robot/commands/RaiseTopClimber.java
+++ src/main/java/frc/robot/commands/RaiseTopClimber.java
@@ -29,7 +29,9 @@ public class RaiseTopClimber extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    climber.setSpeedBottom(0.0);
+  }
 
   // Returns true when the command should end.
   @Override
